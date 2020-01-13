a2e5b8eaeefc7c76101d973a803e2ccdedde4507
alpine12 <iqballecip@gmail.com>
2020-01-13 11:13:56 +0700

    Add Layout Home

--- app/src/main/java/com/example/joynappclient/ui/main_menu/MainMenuActivity.java
+++ app/src/main/java/com/example/joynappclient/ui/main_menu/MainMenuActivity.java
@@ -3,8 +3,14 @@ package com.example.joynappclient.ui.main_menu;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.joynappclient.R;
+import com.example.joynappclient.ui.main_menu.home.HomeFragment;
+
+import java.util.Objects;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -14,7 +20,20 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
 
+        setFragment(HomeFragment.getInstance());
+
     }
 
-
+    private void setFragment(Fragment fragment) {
+        String tag = fragment.getClass().getSimpleName();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (fm.findFragmentByTag(tag) == null) {
+            ft.add(R.id.containerMain, fragment, tag)
+                    .addToBackStack(tag)
+                    .commit();
+        } else {
+            ft.show(Objects.requireNonNull(fm.findFragmentByTag(tag))).commit();
+        }
+    }
 }
