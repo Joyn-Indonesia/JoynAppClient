39690c1c0dbab6bb8f69b599a13415a0a236f4a0
alpine12 <iqballecip@gmail.com>
2020-01-21 20:08:32 +0700

    Update OTP Layout

--- app/src/main/java/com/example/joynappclient/ui/authentication/otp/OtpActivity.java
+++ app/src/main/java/com/example/joynappclient/ui/authentication/otp/OtpActivity.java
@@ -1,43 +1,40 @@
 package com.example.joynappclient.ui.authentication.otp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.joynappclient.R;
 import com.example.joynappclient.ui.authentication.welcome_to_app.WelcomeToAppActivity;
-import com.google.android.material.button.MaterialButton;
+import com.example.joynappclient.utils.MoveActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class OtpActivity extends AppCompatActivity {
     private static final String TAG = "OtpActivity";
 
     //widget
-    @BindView(R.id.btn_next)
-    MaterialButton next;
 
-    //var
+    //vars
     private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
-
-        context = this;
         ButterKnife.bind(this);
+        context = this;
+    }
 
-        next.setOnClickListener(v -> {
-                    // MoveActivity.MoveAct(context, MainMenuActivity.class);
-                    Intent i = new Intent(context, WelcomeToAppActivity.class);
+    @OnClick(R.id.btn_changeNumber)
+    public void changeNumber() {
+        finish();
+    }
 
-                    startActivity(i);
-
-                }
-        );
+    @OnClick(R.id.btn_next)
+    public void next() {
+        MoveActivity.MoveAct(context, WelcomeToAppActivity.class);
     }
 }
